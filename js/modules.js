00967113733365e504d4b1fd0bbee4aa1dd9ce05
tizz98 <dev.tizz98@gmail.com>
2014-10-18 13:48:25 -0400

    stocks in modal now editable

--- js/modules.js
+++ js/modules.js
@@ -8,12 +8,27 @@ function changeStocks() {
 
 window.onload = function () {
 	$('.s_edit').click(function(event){
+
 		if ($(this).hasClass('fa-pencil')) {
+			// edit
 			$(this).removeClass('fa-pencil');
 			$(this).addClass('fa-save');
+			var elem = $(this).closest('.portlet').children('h3')[0];
+
+			var text = elem.innerHTML;
+			console.log(text);
+
+			$(elem).replaceWith("<input type='edit' value='" + text + "'>");
+
 		} else {
+			// save
 			$(this).removeClass('fa-save');
 			$(this).addClass('fa-pencil');
+			var elem = $(this).closest('.portlet').children('input')[0];
+
+			var text = $(elem).val();
+
+			$(elem).replaceWith('<h3 class="portlet-content stock_name">' + text + '</h3>');
 		}
 	});
 }
