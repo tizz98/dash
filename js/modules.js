f5a597712e1b7fd56085f91cd65c11b0de6bbabe
tizz98 <dev.tizz98@gmail.com>
2014-10-18 13:11:32 -0400

    updated stocks to have edit button

--- js/modules.js
+++ js/modules.js
@@ -5,3 +5,15 @@ function setTime(hours) {
 function changeStocks() {
 	$('#stocks_modal').modal('show');
 }
+
+window.onload = function () {
+	$('.s_edit').click(function(event){
+		if ($(this).hasClass('fa-pencil')) {
+			$(this).removeClass('fa-pencil');
+			$(this).addClass('fa-save');
+		} else {
+			$(this).removeClass('fa-save');
+			$(this).addClass('fa-pencil');
+		}
+	});
+}
