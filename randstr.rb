775098e9311e9d5d0fa24ae32862a6f0cd30700f
tizz98 <dev.tizz98@gmail.com>
2014-10-20 21:01:22 -0400

    updated randstr to gen 10 chars

--- randstr.rb
+++ randstr.rb
@@ -1,4 +1,5 @@
+# 107,518,933,731 unique combinations
 o = [('a'..'z'), ('A'..'Z'), (0..9)].map { |i| i.to_a }.flatten
-string = (0..8).map { o[rand(o.length)] }.join
+string = (0..10).map { o[rand(o.length)] }.join
 
 puts string
